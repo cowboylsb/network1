149ea536e914311dd5f53f8e6a7649996cf7a5cd
cowboylsb <cowboylsb57@gmail.com>
2024-07-13 15:56:15 +0900

    first commit

---
+++ src/main/java/servernet/test/User.java
@@ -0,0 +1,34 @@
+package servernet.test;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "test")
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private int age;
+
+// 생성자
+    public User() {}
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
