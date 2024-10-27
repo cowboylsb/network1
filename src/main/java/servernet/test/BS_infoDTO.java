46e595f8392f16253238296b7086c9aeb2aff72d
cowboylsb <cowboylsb57@gmail.com>
2024-10-27 13:09:57 +0900

    BS_v2

--- src/main/java/servernet/test/BS_infoDTO.java
+++ src/main/java/servernet/test/BS_infoDTO.java
@@ -6,13 +6,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BS_infoDTO {
-    private String id;
+    private String CI;
+    private String PCI;
     private int frequency;
-    private int rsrp;
+    private int RSRP;
+    private int RSRQ;
+    private int SINR;
 
-    public BS_infoDTO(String id, int frequency, int rsrp) {
-        this.id = id;
+    public BS_infoDTO(String CI, String PCI, int frequency, int RSRP, int RSRQ, int SINR) {
+        this.CI = CI;
+        this.PCI = PCI;
         this.frequency = frequency;
-        this.rsrp = rsrp;
+        this.RSRP = RSRP;
+        this.RSRQ = RSRQ;
+        this.SINR = SINR;
     }
 }
